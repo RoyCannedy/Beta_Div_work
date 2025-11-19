2756f6d3c6f0e866dd8ff557a48432bece0db841
RoyCannedy <146491787+RoyCannedy@users.noreply.github.com>
2025-11-19 12:26:56 -0600

    Update beta_cancer.R

--- beta_cancer.R
+++ beta_cancer.R
@@ -1,19 +1,8 @@
 library(bnlearn)
 set.seed(42)
 
-ensure_all_factors <- function(df) {
-  for (nm in names(df)) {
-    if (!is.factor(df[[nm]])) df[[nm]] <- factor(df[[nm]])
-  }
-  df
-}
-
 beta_div_limit_zero <- function(node, parents, data, alpha = 1.0) {
-  stopifnot(is.character(node), length(node) == 1, node %in% names(data))
-  data <- ensure_all_factors(data)
-  if (length(parents) == 0) parents <- character(0)
-  
-  node_values <- levels(data[[node]])
+  node_values <- unique(data[[node]])
   n_values <- length(node_values)
   total_score <- 0
   
@@ -22,12 +11,13 @@ beta_div_limit_zero <- function(node, parents, data, alpha = 1.0) {
       obs <- data[[node]][t]
       prior <- if (t == 1) setNames(rep(0, n_values), node_values)
       else table(factor(data[[node]][1:(t-1)], levels = node_values))
+      
+      y_j <- prior[obs]
       y_bar <- sum(prior)
       alpha_bar <- alpha * n_values
       if (y_bar + alpha_bar > 0) {
-        p_vec <- (as.numeric(prior) + alpha) / (y_bar + alpha_bar)
-        names(p_vec) <- node_values
-        total_score <- total_score + log(p_vec[as.character(obs)])
+        p_j <- (y_j + alpha) / (y_bar + alpha_bar)
+        total_score <- total_score + log(p_j)
       }
     }
   } else {
@@ -35,37 +25,30 @@ beta_div_limit_zero <- function(node, parents, data, alpha = 1.0) {
     for (t in seq_len(nrow(data))) {
       obs <- data[[node]][t]
       config <- parent_configs[t]
-      if (t == 1) {
-        prior <- setNames(rep(0, n_values), node_values)
-      } else {
+      prior <- if (t == 1) setNames(rep(0, n_values), node_values)
+      else {
         idx <- parent_configs[1:(t-1)] == config
-        if (any(idx)) prior <- table(factor(data[[node]][1:(t-1)][idx], levels = node_values))
-        else prior <- setNames(rep(0, n_values), node_values)
+        if (any(idx)) table(factor(data[[node]][1:(t-1)][idx], levels = node_values))
+        else setNames(rep(0, n_values), node_values)
       }
+      y_j <- prior[obs]
       y_bar <- sum(prior)
       alpha_bar <- alpha * n_values
       if (y_bar + alpha_bar > 0) {
-        p_vec <- (as.numeric(prior) + alpha) / (y_bar + alpha_bar)
-        names(p_vec) <- node_values
-        total_score <- total_score + log(p_vec[as.character(obs)])
+        p_j <- (y_j + alpha) / (y_bar + alpha_bar)
+        total_score <- total_score + log(p_j)
       }
     }
   }
   return(total_score)
 }
 
-
 beta_div_score <- function(node, parents, data, beta = 1.0, alpha = 1.0) {
-  stopifnot(is.character(node), length(node) == 1, node %in% names(data))
-  data <- ensure_all_factors(data)
-  if (!is.factor(data[[node]])) data[[node]] <- factor(data[[node]])
-  if (length(parents) == 0) parents <- character(0)
-  
   if (beta == 0 || beta < 1e-4) {
     return(beta_div_limit_zero(node, parents, data, alpha))
   }
   
-  node_values <- levels(data[[node]])
+  node_values <- unique(data[[node]])
   n_values <- length(node_values)
   total_score <- 0
   
@@ -117,14 +100,12 @@ beta_div_score <- function(node, parents, data, beta = 1.0, alpha = 1.0) {
   return(total_score)
 }
 
-# ---------- 2) Wrapper for hc ----------
 make_beta_wrapper <- function(beta, alpha = 1) {
   function(node, parents, data, args) {
     beta_div_score(node, parents, data, beta = beta, alpha = alpha)
   }
 }
 
-# ---------- 3) Get a CANCER dataset ----------
 get_cancer_data <- function(n = 5000) {
   have_fit <- FALSE
   suppressWarnings({
@@ -139,6 +120,7 @@ get_cancer_data <- function(n = 5000) {
     }
   })
   if (!have_fit) {
+    # Classic 5-node Cancer BN
     levYN <- c("no","yes")
     Pollution <- factor(sample(c("low","high"), n, replace = TRUE, prob = c(0.9, 0.1)))
     Smoker    <- factor(sample(levYN, n, replace = TRUE, prob = c(0.5, 0.5)))
@@ -146,12 +128,15 @@ get_cancer_data <- function(n = 5000) {
     pc <- mapply(function(p, s) {
       if (p == "low"  && s == "no")  0.03 else
         if (p == "low"  && s == "yes") 0.05 else
-          if (p == "high" && s == "no")  0.10 else 0.20
+          if (p == "high" && s == "no")  0.10 else
+            0.20
     }, as.character(Pollution), as.character(Smoker))
     
     Cancer <- factor(ifelse(runif(n) < pc, "yes", "no"), levels = levYN)
+    
     px <- ifelse(Cancer == "yes", 0.9, 0.2)
     Xray <- factor(ifelse(runif(n) < px, "yes", "no"), levels = levYN)
+    
     pd <- ifelse(Cancer == "yes", 0.65, 0.30)
     Dyspnoea <- factor(ifelse(runif(n) < pd, "yes", "no"), levels = levYN)
     
@@ -159,10 +144,10 @@ get_cancer_data <- function(n = 5000) {
   }
 }
 
-cancer_df <- ensure_all_factors(get_cancer_data(n = 5000))
+cancer_df <- get_cancer_data(n = 5000)
 str(cancer_df)
 
-# ---------- 4) Run HC with beta=0, 0.9, 0.01 ----------
+# ---------- 5) Run HC with different betas ----------
 wrapper0   <- make_beta_wrapper(beta = 0.0,  alpha = 1)
 wrapper09  <- make_beta_wrapper(beta = 0.9,  alpha = 1)
 wrapper001 <- make_beta_wrapper(beta = 0.01, alpha = 1)
@@ -171,6 +156,6 @@ net0   <- hc(cancer_df, score = "custom-score", fun = wrapper0,   maxp = 2)
 net09  <- hc(cancer_df, score = "custom-score", fun = wrapper09,  maxp = 2)
 net001 <- hc(cancer_df, score = "custom-score", fun = wrapper001, maxp = 2)
 
-plot(net0,   main = "CANCER | beta = 0.0 (limit to KL)")
+plot(net0,   main = "CANCER | beta = 0.0")
 plot(net09,  main = "CANCER | beta = 0.9")
 plot(net001, main = "CANCER | beta = 0.01")
